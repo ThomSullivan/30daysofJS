18ef233bdea397391352e2f8fd41f0c5b9a14973
ThomSullivan <83840319+ThomSullivan@users.noreply.github.com>
2022-12-31 12:00:44 -0600

    Day one completed

---
+++ Day1/datatypes.js
@@ -0,0 +1,23 @@
+console.log(typeof variable1)
+console.log(typeof variable2)
+console.log(typeof variable3)
+console.log(typeof variable4)
+let variable5
+let variable6
+let variable7
+let variable8
+let variable9 = "Hello there"
+let variable10 = 42
+let variable11 = 3.14
+let variable12 = false
+let firstName = 'Thom'
+let lastName = 'Smith'
+let maritalStatus = false
+let country = 'United States'
+let age = 35
+let firstName1 = 'Thom', lastName1 = 'Smith', maritalStatus1=false, country1='United States', age1=35
+let myAge = 'I am 25 years old.'
+let yourAge = 'You are 30 years old.'
+console.log(myAge)
+console.log(yourAge) 
+
---
+++ Day1/variable.js
@@ -0,0 +1,4 @@
+let variable1 = 'string'
+let variable2 = true
+let variable3
+let variable4 = null
