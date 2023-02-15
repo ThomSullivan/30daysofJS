b881c984e7831bb7c56596d0ca767b1cc4f5574f
ThomSullivan <83840319+ThomSullivan@users.noreply.github.com>
2023-02-15 08:38:25 -0600

    Day 14 complete

---
+++ Day14/scripts/main.js
@@ -0,0 +1,27 @@
+//LEVEL 1
+
+try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.error(err) // we can use console.log() or console.error()
+  } finally {
+    console.log('In any case I will be executed')
+  }
+
+// LEVEL 2
+
+const throwErrorExampleFun = () => {
+    let message
+    let x = prompt('Enter a number: ')
+    try {
+      if (x == '') throw 'empty'
+      if (isNaN(x)) throw 'not a number'
+      x = Number(x)
+      if (x < 5) throw 'too low'
+      if (x > 10) throw 'too high'
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  throwErrorExampleFun()
